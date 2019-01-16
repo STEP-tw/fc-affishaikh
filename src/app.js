bf926eb9de02fd6e81685d998f00ba5c615bf909
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-01-16 12:23:47 +0530

    Served a landingPage.html file through sever.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
-const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+const fs = require('fs');
+const ENCODING = 'utf8';
+const LANDINGPAGE = './src/landingPage.html';
+
+const createHTMLWriter = function(res) {
+  return function(err, html) {
+    res.write(html);
+    res.statusCode = 200;
+    res.end();
+  };
 };
 
-// Export a function that can act as a handler
+const app = (req, res) => {
+  const writeHTML = createHTMLWriter(res);
+  if (req.url === '/') {
+    fs.readFile(LANDINGPAGE, ENCODING, writeHTML);
+  }
+};
 
 module.exports = app;
