6bd9d0348c782e9c775a71707f25af9b1b08a915
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-01-17 13:27:06 +0530

    Modified the app function to a class.
    Made the app.js a framework.
    Removed all the if statements.

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 const app = require('./src/app.js');
 const PORT = process.env.PORT || 8080;
 
-let server = http.createServer(app);
+let server = http.createServer(app.handleRequest.bind(app));
 server.listen(PORT, () => console.log('listening on ', PORT));
--- src/app.js
+++ src/app.js
@@ -1,49 +1,73 @@
 const fs = require('fs');
-const ENCODING = 'utf8';
-const ABELIOPHYLLUM = './src/htmlPages/Abeliophyllum.html';
-const HANDLE_EVENTS = './src/handleEvents.js';
-const AGERATUM = './src/htmlPages/Ageratum.html';
-const LANDINGPAGE = './src/htmlPages/landingPage.html';
-const GUESTBOOK_HTML = './src/htmlPages/Guestbook.html';
-const MAIN_CSS = './src/css/main.css';
-const FLOWER = './src/resources/flowers.jpg';
-const WATER_JAR = './src/resources/water_jar.gif';
 
-const createHTMLWriter = function(res) {
-  return function(err, html) {
-    res.write(html);
-    res.statusCode = 200;
-    res.end();
-  };
+const renderFile = function(res, fileContents) {
+  res.statusCode = 200;
+  res.write(fileContents);
+  res.end();
 };
 
-const app = (req, res) => {
-  const writeHTML = createHTMLWriter(res);
-  if (req.url === '/') {
-    fs.readFile(LANDINGPAGE, ENCODING, writeHTML);
+class Route {
+  constructor(url, resource, handler) {
+    this.url = url;
+    this.handler = handler;
+    this.resource = resource;
   }
-  if (req.url === '/css/main.css') {
-    res.writeHead(200, { 'Content-Type': 'text/css' });
-    fs.readFile(MAIN_CSS, ENCODING, writeHTML);
-  }
-  if (req.url === '/src/resources/flowers.jpg') {
-    fs.readFile(FLOWER, writeHTML);
-  }
-  if (req.url === '/src/htmlPages/Abeliophyllum.html') {
-    fs.readFile(ABELIOPHYLLUM, ENCODING, writeHTML);
-  }
-  if (req.url === '/src/htmlPages/Ageratum.html') {
-    fs.readFile(AGERATUM, ENCODING, writeHTML);
-  }
-  if (req.url === '/src/htmlPages/Guestbook.html') {
-    fs.readFile(GUESTBOOK_HTML, ENCODING, writeHTML);
-  }
-  if (req.url === '/src/resources/water_jar.gif') {
-    fs.readFile(WATER_JAR, writeHTML);
-  }
-  if (req.url === '/src/handleEvents.js') {
-    fs.readFile(HANDLE_EVENTS, ENCODING, writeHTML);
-  }
+}
+
+const findMatching = function(routes, url) {
+  return routes.filter(route => url === route.url);
 };
+
+class App {
+  constructor() {
+    this.routes = [];
+  }
+  addRoute(url, handler, resource) {
+    const route = new Route(url, handler, resource);
+    this.routes.push(route);
+  }
+  handleRequest(req, res) {
+    const matchingRoute = findMatching(this.routes, req.url);
+    if (matchingRoute.length === 0) {
+      res.statusCode = 404;
+      res.write('File not found');
+      res.end();
+      return;
+    }
+    fs.readFile(matchingRoute[0].resource, (err, fileContents) => {
+      renderFile(res, fileContents);
+    });
+  }
+}
+
+const app = new App();
+app.addRoute('/', './src/htmlPages/landingPage.html', renderFile);
+app.addRoute('/css/main.css', './src/css/main.css', renderFile);
+app.addRoute(
+  '/src/htmlPages/Abeliophyllum.html',
+  './src/htmlPages/Abeliophyllum.html',
+  renderFile
+);
+app.addRoute(
+  '/src/htmlPages/Ageratum.html',
+  './src/htmlPages/Ageratum.html',
+  renderFile
+);
+app.addRoute('/src/handleEvents.js', './src/handleEvents.js', renderFile);
+app.addRoute(
+  '/src/htmlPages/Guestbook.html',
+  './src/htmlPages/Guestbook.html',
+  renderFile
+);
+app.addRoute(
+  '/src/resources/flowers.jpg',
+  './src/resources/flowers.jpg',
+  renderFile
+);
+app.addRoute(
+  '/src/resources/water_jar.gif',
+  './src/resources/water_jar.gif',
+  renderFile
+);
 
 module.exports = app;
