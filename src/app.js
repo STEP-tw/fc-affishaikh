21d11f6439afb20d1e496a2d982e192d1b9e35df
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-01-18 14:42:45 +0530

    Added a functionality to submit comments.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,61 @@
 const fs = require('fs');
 
+const parser = function(parameter) {
+  parameter = parameter.split('=');
+  let parameterValue = parameter[1];
+  return parameterValue.replace(/[+]+/g, ' ');
+};
+
+const storeCommentInFile = function(commentDetails) {
+  commentDetails = commentDetails.split('&');
+  let parsedCommentDetails = commentDetails.map(parser);
+  let parsedComment = {};
+  parsedComment.date = new Date().toLocaleString();
+  parsedComment.name = parsedCommentDetails[0];
+  parsedComment.comment = parsedCommentDetails[1];
+  fs.readFile('./src/commentsLog.json', (err, data) => {
+    let comments = JSON.parse(data);
+    comments.unshift(parsedComment);
+    fs.writeFile('./src/commentsLog.json', JSON.stringify(comments), () => {});
+  });
+};
+
+const renderGuestBook = function(res) {
+  console.log('guest book');
+  fs.readFile('./src/htmlPages/Guestbook.html', (err, data) => {
+    res.statusCode = 200;
+    res.write(data);
+    fs.readFile('./src/commentsLog.json', (err, data) => {
+      const comments = JSON.parse(data);
+      for (
+        let commentIndex = 0;
+        commentIndex < comments.length;
+        commentIndex++
+      ) {
+        let comment =
+          comments[commentIndex].date +
+          '\t' +
+          comments[commentIndex].name +
+          '\t' +
+          comments[commentIndex].comment;
+        res.write('<div>' + comment + '</div>');
+      }
+      res.end();
+    });
+  });
+};
+
+const submitComment = function(req, res) {
+  let commentDetails = '';
+  req.on('data', chunk => {
+    commentDetails += chunk;
+  });
+  req.on('end', () => {
+    storeCommentInFile(commentDetails);
+    renderGuestBook(res);
+  });
+};
+
 const renderFile = function(res, fileContents) {
   res.statusCode = 200;
   res.write(fileContents);
@@ -41,9 +97,17 @@ class App {
       res.end();
       return;
     }
-    fs.readFile(matchingRoute[0].resource, (err, fileContents) => {
-      renderFile(res, fileContents);
-    });
+    if (req.method === 'POST') {
+      matchingRoute[0].handler(req, res);
+    }
+    if(req.method === 'GET' && req.url === '/src/htmlPages/Guestbook.html') {
+      renderGuestBook(res);
+    }
+    if (req.method === 'GET' && req.url !== '/src/htmlPages/Guestbook.html') {
+      fs.readFile(matchingRoute[0].resource, (err, fileContents) => {
+        renderFile(res, fileContents);
+      });
+    }
   }
 }
 
@@ -64,7 +128,7 @@ app.get('/src/handleEvents.js', './src/handleEvents.js', renderFile);
 app.get(
   '/src/htmlPages/Guestbook.html',
   './src/htmlPages/Guestbook.html',
-  renderFile
+  renderGuestBook
 );
 app.get(
   '/src/resources/flowers.jpg',
@@ -75,6 +139,11 @@ app.get(
   '/src/resources/water_jar.gif',
   './src/resources/water_jar.gif',
   renderFile
+);
+app.post(
+  '/src/htmlPages/Guestbook.html',
+  './src/htmlPages/Guestbook.html',
+  submitComment
 );
 
 module.exports = app;
